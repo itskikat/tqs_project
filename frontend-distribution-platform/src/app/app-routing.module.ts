9a88e451569a3519c731eb4599d813e7b876044c
margaridasmartins <margarida.martins@ua.pt>
2021-05-28 01:38:16 +0100

    removing useless frontend distribution pages

--- frontend-distribution-platform/src/app/app-routing.module.ts
+++ frontend-distribution-platform/src/app/app-routing.module.ts
@@ -1,13 +1,5 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import {
-  NbAuthComponent,
-  NbLoginComponent,
-  NbLogoutComponent,
-  NbRegisterComponent,
-  NbRequestPasswordComponent,
-  NbResetPasswordComponent,
-} from '@nebular/auth';
 import { LoginComponent } from './login/login.component';
 import {RegisterProviderComponent} from './register-provider/register-provider.component';
 import {ProviderServiceListComponent} from './provider-service-list/provider-service-list.component';
@@ -20,42 +12,6 @@ import { ProviderRequestsListComponent } from './provider-requests-list /provide
 
 
 export const routes: Routes = [
-  {
-    path: 'pages',
-    loadChildren: () => import('./pages/pages.module')
-      .then(m => m.PagesModule),
-  },
-  {
-    path: 'auth',
-    component: NbAuthComponent,
-    children: [
-      {
-        path: '',
-        component: NbLoginComponent,
-      },
-      {
-        path: 'login',
-        component: NbLoginComponent,
-      },
-      {
-        path: 'register',
-        component: NbRegisterComponent,
-      },
-      {
-        path: 'logout',
-        component: NbLogoutComponent,
-      },
-      {
-        path: 'request-password',
-        component: NbRequestPasswordComponent,
-      },
-      {
-        path: 'reset-password',
-        component: NbResetPasswordComponent,
-      },
-      
-    ],
-  },
   {
     path: 'login', component: LoginComponent
   },
@@ -84,7 +40,7 @@ export const routes: Routes = [
     path: 'regist/business', component: RegisterBusinessComponent
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'pages' },
+  { path: '**', redirectTo: 'login' },
 ];
 
 const config: ExtraOptions = {
