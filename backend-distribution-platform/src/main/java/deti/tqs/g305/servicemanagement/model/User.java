5072987dd4fb1b28adae72d1b8a4c88092543425
Francisca Barros <kikas4323@gmail.com>
2021-06-01 15:33:02 +0100

    added User model

---
+++ backend-distribution-platform/src/main/java/deti/tqs/g305/servicemanagement/model/User.java
@@ -0,0 +1,37 @@
+package deti.tqs.g305.servicemanagement.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name="USER")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="user_type", discriminatorType = DiscriminatorType.STRING)
+public class User {
+
+    @Id
+    @Column(name = "googleID", unique = true)
+    private String google_id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "full_name")
+    private String full_name;
+
+    public User() {
+
+    }
+
+    public User(String google_id, String username, String email, String full_name) {
+        this.google_id = google_id;
+        this.username = username;
+        this.email = email;
+        this.full_name = full_name;
+    }
+}
