857589f86d92e4e1c33d128adc11f7be239a7131
margaridasmartins <margarida.martins@ua.pt>
2021-06-04 03:50:32 +0100

    tests pass

--- backend-distribution-platform/src/test/java/deti/tqs/g305/servicemanagement/restcontroller/ClientRestControllerUnitTest.java
+++ backend-distribution-platform/src/test/java/deti/tqs/g305/servicemanagement/restcontroller/ClientRestControllerUnitTest.java
@@ -1,17 +1,27 @@
 package deti.tqs.g305.servicemanagement.restcontroller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.mockito.Mockito.*;
-
-
+import org.mockito.Mockito;
+import org.mockito.Mock;  
+import java.io.IOException;
 import java.util.Date;
 import java.util.Optional;
 
 import org.mockito.internal.verification.VerificationModeFactory;
 
+import org.springframework.http.MediaType;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import deti.tqs.g305.servicemanagement.model.ServiceContract;
 import deti.tqs.g305.servicemanagement.model.ServiceStatus;
@@ -19,11 +29,10 @@ import deti.tqs.g305.servicemanagement.model.ProviderService;
 import deti.tqs.g305.servicemanagement.model.BusinessService;
 import deti.tqs.g305.servicemanagement.model.Client;
 import deti.tqs.g305.servicemanagement.service.ServiceService;
+import deti.tqs.g305.servicemanagement.JsonUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 
@@ -40,47 +49,51 @@ public class ClientRestControllerUnitTest {
     private ServiceService serviceService;
 
     @Test
-    public void whenPostValidServiceContract_thenCreateServiceContract( ) {
+    public void whenPostValidServiceContract_thenCreateServiceContract( ) throws IOException, Exception {
         
         ServiceContract sc = new ServiceContract(new BusinessService(), new ProviderService(), ServiceStatus.Waiting, new Client(),0);
-        when( serviceService.saveServiceContract(any())).thenReturn(sc);
+        when( serviceService.saveServiceContract(sc)).thenReturn(sc);
 
-        with().body(sc).when().request("POST", "/api/clients/contracts/new").then().statusCode(201)
-        .assertThat().body("status", equalTo("Waiting"));
+        mvc.perform(post("/api/clients/contracts").contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJson(sc)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.status", is("Waiting")));
 
         verify(serviceService, times(1)).saveServiceContract(any());
     }
 
     @Test
-    public void whenPostInvalidServiceContract_thenReturnBadRequest( ) {
+    public void whenPostInvalidServiceContract_thenReturnBadRequest( ) throws IOException, Exception {
 
-        with().body("ups").when().request("POST", "/api/clients/contracts/new").then().statusCode(400);
+        mvc.perform(post("/api/clients/contracts").contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJson("ups")))
+        .andExpect(status().isBadRequest());
 
         verify(serviceService, times(0)).saveServiceContract(any());
     }
 
 
     @Test
-    public void whenPutValidServiceContract_thenUpdateServiceContract( ) {
+    public void whenPutValidServiceContract_thenUpdateServiceContract( ) throws IOException, Exception {
 
         ServiceContract sc = new ServiceContract(new BusinessService(), new ProviderService(), ServiceStatus.Waiting, new Client(),0);
         sc.setReview(2);
         sc.setId(1);
 
         when( serviceService.updateServiceContract(anyLong(),any())).thenReturn(Optional.of(sc));
-
-        with().body(sc).when().request("PUT", "/api/clients/contracts/1").then().statusCode(200)
-        .assertThat().body("review", equalTo(2));
-
-        verify(serviceService, times(1)).updateServiceContract(any(),any()); 
+        
+        mvc.perform(put("/api/clients/contracts/1").contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJson(sc)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.review", is(2)));
+        
+        verify(serviceService, times(1)).updateServiceContract(anyLong(),any()); 
     }
 
     @Test
-    public void whenPutIValidServiceContract_thenReturnBadRequest( ) {
+    public void whenPutIValidServiceContract_thenReturnBadRequest( ) throws IOException, Exception {
 
-        with().body("upsie").when().request("PUT", "/api/clients/contracts/1").then().statusCode(400);
-        
-        verify(serviceService, times(0)).updateServiceContract(any(),any());
+        mvc.perform(put("/api/clients/contracts/1").contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJson("upsie")))
+        .andExpect(status().isBadRequest());
+       
+        verify(serviceService, times(0)).updateServiceContract(anyLong(),any());
     }
 
 
