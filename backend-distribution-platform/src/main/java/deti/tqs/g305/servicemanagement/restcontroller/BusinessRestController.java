9c9a3c33e94d5c6045c0c88c597a193280973a9c
Francisca Barros <kikas4323@gmail.com>
2021-06-04 21:28:27 +0100

    tests passing; possible implementation

--- backend-distribution-platform/src/main/java/deti/tqs/g305/servicemanagement/restcontroller/BusinessRestController.java
+++ backend-distribution-platform/src/main/java/deti/tqs/g305/servicemanagement/restcontroller/BusinessRestController.java
@@ -1,4 +1,45 @@
 package deti.tqs.g305.servicemanagement.restcontroller;
 
+import deti.tqs.g305.servicemanagement.model.Business;
+import deti.tqs.g305.servicemanagement.model.BusinessService;
+import deti.tqs.g305.servicemanagement.model.ServiceContract;
+import deti.tqs.g305.servicemanagement.service.ServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+/**
+ * BusinessRestController
+ */
+
+@RestController
+@RequestMapping("/api/businesses")
 public class BusinessRestController {
+
+    @Autowired
+    private ServiceService serviceService;
+
+    @PostMapping("/services")
+    public ResponseEntity<?> createBusinessService(@RequestBody(required = false) BusinessService bs){
+        if(bs != null){
+            bs = serviceService.saveBusinessService(bs);
+            return new ResponseEntity<BusinessService>(bs, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>("Bad Business Service!", HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping("/services/{id}")
+    public ResponseEntity<?> updateServiceContract(@PathVariable(value = "id") Long businessServiceId, @RequestBody(required = false) BusinessService bs){
+        if(bs != null){
+            Optional<BusinessService> optBs = serviceService.updateBusinessService(businessServiceId, bs);
+            if(optBs.isPresent()){
+                return new ResponseEntity<BusinessService>(bs, HttpStatus.OK);
+            }
+            return new ResponseEntity<String>("Could not find requested business service", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<String>("Bad Business Service!", HttpStatus.BAD_REQUEST);
+    }
 }
