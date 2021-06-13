1e6c52b36d1c590916ac5459e40310540ed14fa2
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-06-13 17:02:49 +0100

    login tutorial set up
    From video 28 https://www.youtube.com/watch?v=Q1839NnaYMY to 52 (not every video, but most of them cover this topic)

--- frontend-distribution-platform/src/app/auth/auth.module.ts
+++ frontend-distribution-platform/src/app/auth/auth.module.ts
@@ -5,13 +5,19 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { FormsModule } from '@angular/forms';
+import { NbCardModule, NbLayoutModule } from '@nebular/theme';
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ResetPasswordComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    // Nebular
+    NbLayoutModule,
+    NbCardModule,
   ],
   exports: [
     LoginComponent, 
--- frontend-distribution-platform/src/app/auth/components/login/login.component.ts
+++ frontend-distribution-platform/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'ngx-login',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    // Create observer to login
+    const loginObserver = {
+      next: x => console.log("User logged in!"),
+      error: err => console.log(err)
+    }
+    this.authService.login(f.value).subscribe(loginObserver);
   }
 
 }
