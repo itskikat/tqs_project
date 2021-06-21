9799ac7bc1503be046e44ad6b31ce8dc1711e8a1
Francisca Barros <kikas4323@gmail.com>
2021-06-21 18:40:01 +0100

    Fixes on client model

--- backend-distribution-platform/src/main/java/deti/tqs/g305/servicemanagement/model/Client.java
+++ backend-distribution-platform/src/main/java/deti/tqs/g305/servicemanagement/model/Client.java
@@ -15,6 +15,9 @@ public class Client extends User {
     @Column(name = "address")
     private String address;
 
+    @Column(name = "location_city")
+    private City location_city;
+
     @Column(name = "birthdate")
     private LocalDate birthdate;
 
@@ -27,9 +30,10 @@ public class Client extends User {
 
     }
 
-    public Client(String email, String password, String full_name, String address, LocalDate birthdate) {
+    public Client(String email, String password, String full_name, String address,  City location_city, LocalDate birthdate) {
         super(email, full_name, password);
         this.address = address;
+        this.location_city = location_city;
         this.birthdate = birthdate;
     }
 }
