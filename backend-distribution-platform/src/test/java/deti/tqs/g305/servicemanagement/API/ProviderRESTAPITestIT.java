349526398236dc222040c6c43bf53c72cef4600f
Francisca Barros <kikas4323@gmail.com>
2021-06-23 18:44:35 +0100

    Clean up to fix

--- backend-distribution-platform/src/test/java/deti/tqs/g305/servicemanagement/API/ProviderRESTAPITestIT.java
+++ backend-distribution-platform/src/test/java/deti/tqs/g305/servicemanagement/API/ProviderRESTAPITestIT.java
@@ -4,6 +4,7 @@ import deti.tqs.g305.servicemanagement.configuration.JwtTokenUtil;
 import deti.tqs.g305.servicemanagement.model.*;
 import deti.tqs.g305.servicemanagement.repository.*;
 import deti.tqs.g305.servicemanagement.service.UserService;
+import org.junit.jupiter.api.AfterEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -76,6 +77,15 @@ public class ProviderRESTAPITestIT {
 
     @Autowired
     private UserService userService;
+
+    @AfterEach
+    void cleanUp() {
+        serviceContractRepository.deleteAll();
+        businessServiceRepository.deleteAll();
+        providerServiceRepository.deleteAll();
+        serviceTypeRepository.deleteAll();
+        userRepository.deleteAll();
+    }
 
     @Test
     void whenGetContracts_thenReturnContracts() {
